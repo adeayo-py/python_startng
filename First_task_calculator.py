08f3333d65132ec1867cfeaff1718162953688e8
adeayo-py <61404143+adeayo-py@users.noreply.github.com>
2020-03-24 22:13:30 +0100

    Calculator for area of circle

---
+++ First_task_calculator.py
@@ -0,0 +1,12 @@
+"""
+Write a Python program which accepts the 
+radius of a circle from the user and computes 
+the area.
+"""
+
+radius=input("Input the circle radius: ")
+r=int(radius)
+pi=3.142
+
+circle_area=pi*(r**2)
+print ("The area of the circle is",circle_area)
